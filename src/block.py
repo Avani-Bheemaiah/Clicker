b1c057100111ec36a17e8c6175a76c66e2031f77
Avani <maru@MacBook-Pro.attlocal.net>
2022-06-30 17:26:29 -0700

    clicker

--- src/block.py
+++ src/block.py
@@ -4,19 +4,27 @@ class Block(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprite_sheet = pygame.image.load('./resources/Spritesheet.png')
+        
         self.load_sprites()
-        self.sprite = self.sprites['Dirt']
+        self.sprite = self.sprites[0]
+        self.number = 0
+        
         
     def load_sprites(self):
-        self.sprites = {} 
+        self.sprites = []
         #x, y, w, h
-        self.sprites['Dirt'] = util.load_sprite(self.sprite_sheet, 310, 27, 73, 73)
-        self.sprites['Grass'] = util.load_sprite(self.sprite_sheet, 237, 27, 73, 73)
-        self.sprites['Stone'] = util.load_sprite(self.sprite_sheet, 383, 27, 73, 73) 
-        self.sprites['Coal'] = util.load_sprite(self.sprite_sheet, 310, 173, 73, 73)
-        self.sprites['Gold'] = util.load_sprite(self.sprite_sheet, 456, 100, 73, 73)
-        self.sprites['Redstone'] = util.load_sprite(self.sprite_sheet, 602, 100, 73, 73)
-        self.sprites['Diamond'] = util.load_sprite(self.sprite_sheet, 529, 100, 73, 73)
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 310, 27, 73, 73))
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 237, 27, 73, 73))
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 383, 27, 73, 73))
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 310, 173, 73, 73))
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 456, 100, 73, 73))
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 602, 100, 73, 73))
+        self.sprites.append(util.load_sprite(self.sprite_sheet, 529, 100, 73, 73))
         
-        for state in self.sprites.keys():
-            self.sprites[state] = pygame.transform.scale2x(self.sprites[state])
+        for i in range (len(self.sprites)):
+            self.sprites[i] = pygame.transform.scale2x(self.sprites[i])
+
+    def upgrade(self):
+        if self.number < 6:
+            self.number+=1
+            self.sprite = self.sprites[self.number]
--- src/main.py
+++ src/main.py
@@ -7,23 +7,33 @@ score = 0
 # innit: gets called when block class called
 # when class created: every function has 'self' keyword => refers to instance of class; TBC ~remind L.
 # 'self' to be covered 4/31
+# A tuple is a list but its contents can't be changed
+
+# 1) Implement block upgrades
+# 2) Make sure it substracts from score
+# 3) Keep track of costs and click power
+
+button = pygame.image.load('./resources/button.png')
+button = pygame.transform.scale(button,(100,100))
+upgrade_cost = 5
+click_power = 3
 
 def init():
+    global screen, font, block
     pg.init()
     screen = pg.display.set_mode((c.WIDTH, c.HEIGHT))
     pg.display.set_caption('Dirt Clicker')
     screen.fill('white')
     print('Beginning event logging...')
     print()
-    block = Block()
-    screen.blit(block.sprite, (400, 300))
+    block = Block()  
     #left top: (400,300)
     #automatically called "__init__"
     print('Game Start')
     font = pg.font.Font(pg.font.get_default_font(),16)
 
 def main():
-    init()
+    init() 
     running = True
     while running:
         for event in pg.event.get():
@@ -31,15 +41,29 @@ def main():
                 running = False
                 
             if event.type == pg.MOUSEBUTTONDOWN:
-                handle_click()
+                handle_click(event.pos)
         pg.display.update()
+        screen.fill("aquamarine")
+        screen.blit(block.sprite, (400, 300))
+        screen.blit(button, (450,200))
+        screen.blit(font.render(str(score),False,"coral1"),(450,100))
+        
     pg.quit()
     print("Window closed, game successfully quit")
 
-def handle_click(x, y):
+def handle_click(pos):
+    global score, click_power, upgrade_cost
+    x,y = pos
     if x in range (400,546) and y in range (300,446):
-        score+=1
+        score+=click_power
         
+    if x in range (450,450 + button.get_width()) and y in range (200, 200 + button.get_height()):
+
+        if score >= upgrade_cost:
+            score-=upgrade_cost
+            click_power*=2
+            upgrade_cost*=2
+            block.upgrade()
         
 
 main()
